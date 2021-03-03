907d6380419620338fc2824b33984f73e5533ebd
Bartosz <bartek.witek@onet.eu>
2021-03-03 16:58:01 +0100

    Added wincondition to minigame#2
    The eggs still spawn even after winning (might have to rework the code) but the gamemode is fully playable now with a goal and a win-condition

--- Assets/Scripts/Minigames/Minigame#2/EggCollectionSpawning.cs
+++ Assets/Scripts/Minigames/Minigame#2/EggCollectionSpawning.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EggCollectionSpawning : MonoBehaviour
 {
@@ -16,29 +17,52 @@ public class EggCollectionSpawning : MonoBehaviour
     private int sizeOfEggSpawnLocationsList; //The max size of the eggSpawnLocationList
     private int randomEggSpawnLocation;//Random Index for the location list
     private int lastSpawn = 9999;//The last spawn location, set high to not predetermine first spawn
+    public static int score = 0;
+    public GameObject wintext;
+    public GameObject goaltext;
+    private float levelcountdown = 0;
 
-    private void Start()
+    void Start()
     {
         sizeOfEggSpawnLocationsList = eggSpawnLocationsList.Count;//Grabs the size of the list
         StartCoroutine(EggSpawner());//Starts the spawning coroutine
+        score = 0;
+        wintext.SetActive(false);
     }
 
     IEnumerator EggSpawner()
     {
-        for (int i = 0; i < eggSpawnAmount; i++)
+            for (int i = 0; i < eggSpawnAmount; i++)
+            {
+                randomEggSpawnLocation = Random.Range(0, sizeOfEggSpawnLocationsList);//Gives a random spawn location Index
+                if (lastSpawn == randomEggSpawnLocation && randomEggSpawnLocation == sizeOfEggSpawnLocationsList) { randomEggSpawnLocation++; } //If the last spawn was the same as the new random, and it is the highest number -1
+                else if (lastSpawn == randomEggSpawnLocation && randomEggSpawnLocation == 0) { randomEggSpawnLocation++; } //If the last spawn was the same as the new random one, and it is the lowest number +1
+
+                Transform spawnLocation = eggSpawnLocationsList[randomEggSpawnLocation].transform; //Grab the transform of the random location from the list
+                GameObject Egg = Instantiate(eggPrefab, transform);//Instantiate the Egg prefab
+                Egg.transform.position = spawnLocation.position;//Set the Egg location to the random location
+
+                randomDelayForSpawn = Random.Range(delayBetweenSpawnsMin, delayBetweenSpawnsMax);
+
+                lastSpawn = randomEggSpawnLocation;//Set the last spawn to the current latest spawn
+                yield return new WaitForSeconds(randomDelayForSpawn);//Wait for random time for next egg
+            }
+    }
+
+    void Update()
+    {
+        if(score == 5)
         {
-            randomEggSpawnLocation = Random.Range(0, sizeOfEggSpawnLocationsList);//Gives a random spawn location Index
-            if(lastSpawn == randomEggSpawnLocation && randomEggSpawnLocation == sizeOfEggSpawnLocationsList) { randomEggSpawnLocation++; } //If the last spawn was the same as the new random, and it is the highest number -1
-            else if(lastSpawn == randomEggSpawnLocation && randomEggSpawnLocation == 0) { randomEggSpawnLocation++; } //If the last spawn was the same as the new random one, and it is the lowest number +1
-
-            Transform spawnLocation = eggSpawnLocationsList[randomEggSpawnLocation].transform; //Grab the transform of the random location from the list
-            GameObject Egg = Instantiate(eggPrefab, transform);//Instantiate the Egg prefab
-            Egg.transform.position = spawnLocation.position;//Set the Egg location to the random location
-
-            randomDelayForSpawn = Random.Range(delayBetweenSpawnsMin, delayBetweenSpawnsMax);
-
-            lastSpawn = randomEggSpawnLocation;//Set the last spawn to the current latest spawn
-            yield return new WaitForSeconds(randomDelayForSpawn);//Wait for random time for next egg
+            Win();
         }
+    }
+    public void Win()
+    {
+        goaltext.SetActive(false);
+        wintext.SetActive(true);
+        levelcountdown += Time.deltaTime;
+
+        if (levelcountdown >= 5)
+            SceneManager.LoadScene("CoreGame");
     }
 }
