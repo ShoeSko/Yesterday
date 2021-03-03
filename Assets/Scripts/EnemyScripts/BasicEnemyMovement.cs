55138fab944b59cd82b920677ad3c42b39628d57
Sebastian K Øie <sebagnus@gmail.com>
2021-03-03 20:22:22 +0100

    Interaction between Good & Evil
    Basic tower/Unit made,
    Has health
    If shooter has
    Shooting recharge time
    Shooting speed
    Shooting projectile
    Shooting Damage (Might change the order of these later)
    What layer is the enemy?
    What layer is the blocked range box?
    
    Enemies now can attack
    With added attack damage
    Attack speed
    What layer the unit is on?
    What is the attack range?
    And the position the attack comes from.
    
    As well as some prefabs & colour changes to the Core game scene for visual understanding.

--- Assets/Scripts/EnemyScripts/BasicEnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/BasicEnemyMovement.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class BasicEnemyMovement : MonoBehaviour
 {
-
     [Header("Enemy Controls")]
-    [Range(0,100)]public float moveSpeed = 5;
+    [Range(0,100)][SerializeField]private float moveSpeed = 5;
+    [Range(0,100)][SerializeField] private int enemyHealth;
+    [Tooltip("The tag corresponding to what the enemy will care about")][SerializeField]private string obstacleTags;
+    [Tooltip("The tag coresponding to projectiles")][SerializeField]private string projectileTags;
 
-    [Range(0,100)]public float enemyHealth;
+    [Header("Enemy Attack")]
+    [Tooltip("The damage they deal")][Range(0,100)][SerializeField]private int attackDamage;
+    [Tooltip("The rate of attacks")][Range(0, 100)] [SerializeField]private float attackSpeed;
+    [Tooltip("What layer is to be attacked?")][SerializeField]private LayerMask whatIsUnitLayer;
+    [Tooltip("How big is the attack range?")][SerializeField]private float attackRange;
+    [Tooltip("Where does it attack from?")][SerializeField]private Transform attackPosition;
 
-    [Tooltip("The tag corresponding to what the enemy will care about")]public string obstacleTags;
-
-    private bool obstacleInTheWay = false;
+    private bool obstacleInTheWay;//Is there a unit blocking the path?
     private Rigidbody2D rg2D;
+    private bool hasAttacked; //Has it attacked, wait until ready again.
+
     private void Start()
     {
         rg2D = GetComponent<Rigidbody2D>();
@@ -21,40 +28,76 @@ public class BasicEnemyMovement : MonoBehaviour
 
     private void Update()
     {
-        MovingEnemy();
-        EnemyDeath();
+        MovingEnemy();//Currently both moving & attacking
+        EnemyDeath();//Death comes for us all.
     }
 
     void MovingEnemy()
     {
         if (!obstacleInTheWay)//If there are no obstacles the enemy will start moving
         {
-            rg2D.velocity = new Vector2(-moveSpeed * Time.deltaTime, 0);
+            rg2D.velocity = new Vector2(-moveSpeed * Time.deltaTime, 0); //Move to the right timed with deltatime for now, have to check build if change has to be done.
         }
-        else { /*Whatever "harm" the enemy will do*/}
+        else { AttackObstacle(); } //If you have someone blocking your path, kill them.
     }
 
     private void OnCollisionEnter2D(Collision2D other) //Stops movement upon reaching a blockade
     {
-        if (other.collider.CompareTag(obstacleTags))
+        if (other.collider.CompareTag(obstacleTags)) //Is there an obstacle blocking your path.
         {
             obstacleInTheWay = true;
         }
     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(projectileTags))
+        {
+            UnitPrototypeScript parentScript = other.gameObject.GetComponentInParent<UnitPrototypeScript>();
+            enemyHealth = enemyHealth - parentScript.projectileDamage;//Make a way to transfer set damage.
+            Destroy(other.gameObject);//Current issue for later, bullet takes time to dissapear.
+        }
+    }
 
-    private void OnCollisionExit2D(Collision2D other) //Restarts movement upon reaching a blockade
+    private void OnCollisionExit2D(Collision2D other) //Restarts movement upon destroying the obstacle
     {
         if (other.collider.CompareTag(obstacleTags))
         {
             obstacleInTheWay = false;
+        }        
+    }
+    
+    private void AttackObstacle() //Attack time
+    {
+        if (!hasAttacked) //If you have yet to attack
+        {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition.position,attackRange, whatIsUnitLayer); //Overlap of all Units withing the attack range
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                enemiesToDamage[i].GetComponent<UnitPrototypeScript>().TakeDamage(attackDamage); //Sent attackDamage to Unit
+            }
         }
+        else if (hasAttacked) { StartCoroutine(AttackRecharge()); } //Start Coroutine to recharge
     }
 
-    private void EnemyDeath()
+    private void OnDrawGizmosSelected() //Gizmo to represent attack range.
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+    }
+    IEnumerator AttackRecharge() //Recharges the attack speed
+    {
+        yield return new WaitForSeconds(attackSpeed);
+        hasAttacked = false;
+        yield return null;
+    }
+
+    private void EnemyDeath()//If there is no more health, die.
     {
         if (enemyHealth <= 0)
         {
             Destroy(gameObject);
         }
     }
+
+    
 }
--- Assets/Scripts/UnitScripts/UnitPrototypeScript.cs
+++ Assets/Scripts/UnitScripts/UnitPrototypeScript.cs
@@ -4,56 +4,73 @@ using UnityEngine;
 
 public class UnitPrototypeScript : MonoBehaviour
 {
-    /*
-     HP for destruction
-    
-    A shoot function
-        Bool if it is a shooter or not
-        public prefab placement for customization
-        Fire rate
-        Fire speed
-        Fire direction
-        Damage.. on bullet perhaps
-        
-    Physical harm
-        Bool if physical harmer or not
-        attack speed
-        Attack damage, if applicable
-        
-      
-     
-     */
+    [Header("Base Stats")]
+    [Range(0,100)]public float health;
 
     [Header("Shooting")]
-    [Tooltip("Is this unit going to shoot?")]public bool isShooter;
-    private bool isRecharging;
-    [Tooltip("Time between each shot")]public float shootRechargeTime;
-    public float projectileSpeed;
-    [Tooltip("Prefab of the projectile to be shot")]public GameObject projectilePrefab; //Find a way to make this not mandatory...
+    [Tooltip("Is this unit going to shoot?")][SerializeField]private bool isShooter;
+    [Tooltip("Time between each shot")][Range(0,100)][SerializeField] private float shootRechargeTime;
+    [Tooltip("The speed of the projectile being fired")][Range(0,100)][SerializeField]private float projectileSpeed;
+    [Tooltip("Prefab of the projectile to be shot")][SerializeField] private GameObject projectilePrefab; //Find a way to make this not mandatory? Or make 2 scripts, one range & one melee.
+    [Tooltip("The damage the projectile will deal")][Range(0,100)] public int projectileDamage;
+    [SerializeField]private LayerMask targetLayer;
+    [SerializeField]private LayerMask edgeOfRangeLayer;
 
+    private bool isRecharging;
+    private Vector3 originForAim;
+    private Vector3 directionForAim;
+    private float hitRange;
 
     [Header("Hitting")]
     private bool isCQC;
 
+    private void Start()
+    {
+        Aim();
+    }
 
+    private void Update()
+    {
+        ShootProjectile();
+        Death();
+    }
 
 
     private void ShootProjectile()
     {
-        bool temptForIf = true;//To be removed when shoot condition is stable.
-        //Raycast to trigger shooting?
         if (isShooter)
         {
-            if (!isRecharging)
-            {
+            RaycastHit2D hit; //Delegate memory
 
-                isRecharging = true;
-                StartCoroutine(ShootRecharge());
+            hit = Physics2D.Raycast(originForAim, directionForAim, hitRange, targetLayer);
+            Debug.DrawRay(originForAim, directionForAim * hitRange, Color.red,10);
+            if (hit)
+            {
+                //Debug.Log(hit.collider.name);//What is hit
+                //Debug.DrawLine(originForAim, hit.transform.position, Color.blue); //Line to target
+                if (!isRecharging)
+                {
+                    GameObject projectileObject = Instantiate(projectilePrefab, transform);
+                    Rigidbody2D projectileObjectRg2D = projectileObject.GetComponent<Rigidbody2D>();
+                    projectileObject.transform.position = transform.position;
+                    projectileObjectRg2D.velocity = new Vector2(projectileSpeed/* * Time.deltaTime*/,0);
+
+                    isRecharging = true;
+                    StartCoroutine(ShootRecharge());
+                }
             }
         }
     }
 
-
+    private void Aim()
+    {
+        originForAim = transform.position;
+        directionForAim = transform.right;
+        RaycastHit2D hitRangeLimit;
+        hitRangeLimit = Physics2D.Raycast(originForAim, directionForAim, 999999, edgeOfRangeLayer);
+        hitRange = hitRangeLimit.distance;
+        Debug.Log(hitRange);
+    }
 
     IEnumerator ShootRecharge()
     {
@@ -61,5 +78,15 @@ public class UnitPrototypeScript : MonoBehaviour
         isRecharging = false;
         Debug.Log("Done Recharging");
         yield return null;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    private void Death()
+    {
+        if(health <= 0) { Destroy(gameObject); }
     }
 }
