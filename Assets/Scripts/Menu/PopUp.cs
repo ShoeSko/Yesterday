1a032bae73a0b165da580a1bd5abe6a18e3d15a6
Jace Thai <itchi@LIVE.NO>
2021-04-23 20:16:00 +0200

    How to play
    UI og knapp how to play menu

---
+++ Assets/Scripts/Menu/PopUp.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PopUp : MonoBehaviour
+{
+    [SerializeField] Button _Button1;
+    [SerializeField] GameObject _Canvas1;
+    [SerializeField] TextMesh _Button1text;
+    [SerializeField] TextMesh _popupText;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Init(Transform canvas, string popupmessage, string button1, System.Action Action)
+    {
+
+    }
+
+}
