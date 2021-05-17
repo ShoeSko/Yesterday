b896c21a6972921dc35dfc437abe4170d4fe4113
Sebastian K Øie <sebagnus@gmail.com>
2021-05-17 15:20:32 +0200

    Upgraded TextMesh Script
    The script can now adjust for Outline as well as colour for individual elements.

--- Assets/Scripts/SceneScripts/TextMeshProColour.cs
+++ Assets/Scripts/SceneScripts/TextMeshProColour.cs
@@ -4,12 +4,26 @@ using TMPro;
 public class TextMeshProColour : MonoBehaviour
 {
     private TextMeshProUGUI text;
+    [Header("Colour of text")]
     public Color colour;
+
+    [Header("Outline of text")]
+    public bool useOutline;
+    public Color outlineColour;
+    [Range(0,1)]public float outlineWitdh;
 
     private void Awake()
     {
         text = GetComponent<TextMeshProUGUI>();
 
         text.color = colour;
+
+        text.faceColor = colour;
+
+        if (useOutline)
+        {
+            text.outlineColor = outlineColour;
+            text.outlineWidth = outlineWitdh;
+        }
     }
 }
