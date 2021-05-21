52e8651aa22f46cd83234415a2c5f263ceea3e95
Sebastian K Øie <sebagnus@gmail.com>
2021-05-21 16:45:08 +0200

    Minigame#11 Switch Indication
    Added an animated highlight to all switches.
    These turn on the normal indication upon that row being relevant, and off when they are done.
    If it is the wrong switch then they will play a brown like version of the animation to indicate it being wrong.

--- Assets/Scripts/Minigames/Minigame#11/FlipTheSwitch.cs
+++ Assets/Scripts/Minigames/Minigame#11/FlipTheSwitch.cs
@@ -4,6 +4,12 @@ public class FlipTheSwitch : MonoBehaviour
 {
     private bool isFlipped;
     private float flipStrenght = -90;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = transform.GetComponentInChildren<Animator>();
+    }
 
     private void OnMouseDown() //Is called with a mouseclick or touch on an object with collider. (Ignore raycast layer prevents this)
     {
@@ -15,12 +21,13 @@ public class FlipTheSwitch : MonoBehaviour
         if (!isFlipped)
         {
             transform.localRotation = Quaternion.Euler(0, 0,flipStrenght); //Flips the object 90 degrees
-            isFlipped = false;
+            isFlipped = true;
+            animator.SetTrigger("Wrong");
         }
         else
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0); //Returns the object to it's original rotation
-            isFlipped = true;
+            isFlipped = false;
         }
     }
 }
