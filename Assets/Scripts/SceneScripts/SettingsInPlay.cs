482b79b0f94fc9b91c875fba2cd3356d34541f4c
Sebastian K Øie <sebagnus@gmail.com>
2021-05-25 23:33:58 +0200

    Settings for all scenes
    All scenes have a settings canvas now (Core game has it turned off for now, because Quacken)

---
+++ Assets/Scripts/SceneScripts/SettingsInPlay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsInPlay : MonoBehaviour
+{
+
+    public void SettingsPressed()
+    {
+        Time.timeScale = 0; //Frezes time (for everything that cares about time...)
+
+    }
+
+    public void LeftSettings()
+    {
+        Time.timeScale = 1; //Resets the time
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
