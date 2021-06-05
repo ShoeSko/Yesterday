c4e563b2e3aa1decd55d6d1e21d74adcaf919f23
Sebastian K Øie <sebagnus@gmail.com>
2021-06-05 18:44:06 +0200

    Mana Hamster V1
    The Basic mana Hamster running has been added, upgraded slider to be a circle running in the hamster wheel. Added all Mana related references to know about the hamster.

--- Assets/Scripts/CoreGame/ManaSystem.cs
+++ Assets/Scripts/CoreGame/ManaSystem.cs
@@ -9,11 +9,17 @@ public class ManaSystem : MonoBehaviour
     public static int MaximumMana = 10;//maximum possible mana
     private float manaGainSpeed = 3f; //How long it takes to gain mana.
 
-    public Slider manaSlider; //Slider to indicate mana gain
     public Text mana;//your current mana
 
     private float time;//clock
     private bool start;
+
+    [Header("Hamster Animation Variables")]
+    [SerializeField] private Animator hamsterAnimation;
+    private bool isHamsterRunning;
+
+    [SerializeField] private Image hamsterCircle;
+
     void Start()
     {
         CurrentMana = 0; // Resets the mana value for a fresh game.
@@ -30,7 +36,24 @@ public class ManaSystem : MonoBehaviour
         if(CurrentMana < MaximumMana && start)
         {
             time += Time.deltaTime;
-            manaSlider.value = time;
+
+            hamsterCircle.fillAmount = time / manaGainSpeed; //Theory of Math. Circle goes to a max of 1, want the time to be represented in this with the circle going at that speed.
+
+            if (!isHamsterRunning)
+            {
+                Debug.Log("I Hamster Now");
+                hamsterAnimation.SetTrigger("Charge");
+                isHamsterRunning = true;
+            }
+        }
+
+        if (CurrentMana >= MaximumMana)
+        {
+            if (isHamsterRunning)
+            {
+                hamsterAnimation.SetTrigger("Full");
+                isHamsterRunning = false;
+            }
         }
 
         if(time >= manaGainSpeed)
