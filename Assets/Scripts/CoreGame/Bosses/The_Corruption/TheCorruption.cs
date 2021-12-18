683f18075ad876b8043f86c8f50b31486007ea46
Bartosz <bartek.witek@onet.eu>
2021-12-18 05:33:36 +0100

    Working on Corruption Boss
    The first ability (Corrupted Grounds) has been implemented and work properly.
    Created a solid backbone for future coding of the boss' abilities

--- Assets/Scripts/CoreGame/Bosses/The_Corruption/TheCorruption.cs
+++ Assets/Scripts/CoreGame/Bosses/The_Corruption/TheCorruption.cs
@@ -27,15 +27,40 @@ public class TheCorruption : MonoBehaviour
     private int CD2;
     private int CD3;
 
+    private int LoopErrorFix;
+
     private int RandomAbility;
 
-    //Ability 1
+    //Ability_Desperate_Rage
 
 
-    //Ability 2
+    //Ability_Corrupted_Grounds
+    private int RandomLane;
+    private bool DeactivateDelay;
+    private float AbilityDelay;
+    private GameObject CurrentTemporarySpotDisable;
+    public GameObject TemporarySpotDisableL1;
+    public GameObject TemporarySpotDisableL2;
+    public GameObject TemporarySpotDisableL3;
+    public GameObject TemporarySpotDisableL4;
+    private GameObject CurrentLaneChange;
+    public GameObject LaneChangeL1;
+    public GameObject LaneChangeL2;
+    public GameObject LaneChangeL3;
+    public GameObject LaneChangeL4;
+
+    //Ability_Corrupting_Gaze
 
 
-    //Ability 3
+    //Ability_Purification
+
+
+
+    //Sound
+    //public List<AudioSource> VoiceCorruptedGrounds = new List<AudioSource>();
+    //public List<AudioSource> VoiceCorruptingGaze = new List<AudioSource>();
+    //public List<AudioSource> VoicePurification = new List<AudioSource>();
+    //public List<AudioSource> VoiceTakeDamage = new List<AudioSource>();
 
 
 
@@ -57,9 +82,14 @@ public class TheCorruption : MonoBehaviour
         NewPos = new Vector2(6.6f, 1.59707f);
         speed = 0.9f;
 
-        Abilities.Add("Wrath_of_the_Forest");//Damage spikes
-        Abilities.Add("Eclipse");//Mist screen coverup
-        Abilities.Add("Motherly_Embrace");//Buff
+        CD2 = 1;
+        CD3 = 3;
+
+        Abilities.Add("Corrupted_Grounds");//Zone Debuff - CD1 (1 Turn CD)
+        Abilities.Add("Corrupting_Gaze");//Special Unit - CD2 (2 Turn CD)
+        Abilities.Add("Purification");//Purify Zone - CD3 (3 Turn CD)
+
+        Abilities.Add("Desperate_Rage");//Card Removal (Passive)
 
         for (int deactivated = 0; deactivated < DeactivateTowerSpots.Count; deactivated++)//make space for the boss by destroying the last row
         {
@@ -79,8 +109,154 @@ public class TheCorruption : MonoBehaviour
 
             if(timer >= 18)//time ebfore boss uses ability;
             {
+                LoopErrorFix = 15;//This will prevent the loop from going infinitely if all abilities are on cooldown (which is possible specifically for this boss, but im not willing to re-design the ability cooldown to fix this)
 
+                for (loop = 0; loop < 1; loop++)
+                {
+                    LoopErrorFix--;
+
+                    if (LoopErrorFix == 0)
+                    {
+                        CD1--;
+                        CD2--;
+                        CD3--;
+                    }
+
+                    RandomAbility = Random.Range(0, 3);//choose one random ability
+                    if (RandomAbility == 0)
+                        Corrupted_Grounds();
+                    else if (RandomAbility == 1)
+                        Corrupting_Gaze();
+                    else if (RandomAbility == 2)
+                        Purification();
+                }
+                Debug.Log("I used" + Abilities[RandomAbility]);
+                CD1--;
+                CD2--;
+                CD3--;
+                timer = 0;
             }
         }
+
+        if (DeactivateDelay)//This is the Corrupted_Grounds Ability
+        {
+            AbilityDelay += Time.deltaTime;
+
+            if(AbilityDelay <= 16)
+            {
+                if(CurrentTemporarySpotDisable != null)
+                    CurrentTemporarySpotDisable.SetActive(false);
+
+                CurrentLaneChange.SetActive(true);
+            }
+            else
+            {
+                CurrentLaneChange.SetActive(false);
+                CurrentTemporarySpotDisable.SetActive(true);
+                DeactivateDelay = false;
+            }
+        }
+
+
+        if (Health == 0)
+            IsDead = true;
+
+        if (IsDead == true)
+        {
+            if (!hasBeenDefeated)
+            {
+                WinCondition.GetComponent<Victory>().Win();
+                //AddBossDefeatToBestiary(); //Sebastian please fix this, i dont know what to do
+                hasBeenDefeated = true;
+            }
+        }
+    }
+
+
+    void Desperate_Rage()//Ability 0 (Soecial Passive)
+    /*The Corruption gets even angrier as it weakens in its power, 
+     * driving it to utter madness. When The Corruption takes damage, 
+     * it uses this ability and corrupts one of your cards from your hand. 
+     * A corrupted card cannot be played.*/
+    {
+        //Do stuff
+    }
+
+    void Corrupted_Grounds()//Ability 1
+    /*The Corruption starts taking over your land, corrupting it to its will. 
+     * One of your lanes starts oozing purple liquid and becomes corrupted for 16 seconds. 
+     * While corrupted in this way, you cannot place any units in that lane until The Corruptions releases its grasp on it.*/
+    {
+        if (CD1 <= 0)
+        {
+            //Do stuff
+            AbilityDelay = 0;
+            RandomLane = Random.Range(0, 5);
+
+            if (RandomLane == 1)
+            {
+                CurrentTemporarySpotDisable = TemporarySpotDisableL1;
+                CurrentLaneChange = LaneChangeL1;
+            }
+            else if (RandomLane == 2)
+            {
+                CurrentTemporarySpotDisable = TemporarySpotDisableL2;
+                CurrentLaneChange = LaneChangeL2;
+            }
+            else if (RandomLane == 3)
+            {
+                CurrentTemporarySpotDisable = TemporarySpotDisableL3;
+                CurrentLaneChange = LaneChangeL3;
+            }
+            else if (RandomLane == 4)
+            {
+                CurrentTemporarySpotDisable = TemporarySpotDisableL4;
+                CurrentLaneChange = LaneChangeL4;
+            }
+
+            DeactivateDelay = true;
+
+            Debug.Log("Im using Lane" + CurrentTemporarySpotDisable);
+
+            CD1 = 2;
+        }
+        else
+            loop--;
+    }
+
+    void Corrupting_Gaze()//Ability 2
+    /*The Corruption's gaze is so powerful it can corrupt any living creature, 
+     * turning it against its own will, under the command of the entity. 
+     * Apart from sending businessmen and beasts, 
+     * The Corruption will corrupt the units from your deck and send them against you.*/
+    {
+        if (CD2 <= 0)
+        {
+            //Do stuff
+
+            CD2 = 3;
+        }
+        else
+            loop--;
+    }
+
+
+    void Purification()//Ability 3
+    /*When fighting The Corruption, its roots start coming out of the ground. 
+     * By sending one of your units to that spot, it can be purified to push back the entity. 
+     * The spot disappears after 5 seconds if no unit is placed within that time. 
+     * When a unit is placed in the "purification zone", it starts the purification process, which takes 15 seconds. 
+     * During that time the unit becomes vulnurable and can't attack. 
+     * In additon, The Corruption will become aggressive and send more units during that period. 
+     * Once the zone is cleansed, The Corruption will take a point of damage and trigger its "Desperate Rage" ability.*/
+    {
+        if (CD3 <= 0)
+        {
+            //Do stuff
+
+            CD3 = 4;
+        }
+        else
+            loop--;
     }
 }
