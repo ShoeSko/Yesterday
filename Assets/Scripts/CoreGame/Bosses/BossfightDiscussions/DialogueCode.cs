e22f1f34d29138055894a7a07d5c99a18a370d8f
Bartosz <bartek.witek@onet.eu>
2022-01-20 07:13:16 +0100

    Preview / Fundamentals for "bossfight dialogues"
    Created an animation-based foundation for soon to be dating-sim-esq talking sequences during the boss fight with the boss' henchmen.
    The game is paused in the meantime and background is dimmed

--- Assets/Scripts/CoreGame/Bosses/BossfightDiscussions/DialogueCode.cs
+++ Assets/Scripts/CoreGame/Bosses/BossfightDiscussions/DialogueCode.cs
@@ -11,13 +11,143 @@ public class DialogueCode : MonoBehaviour
     public GameObject Robot_Casual;
 
     //Various Businessmen
-    public GameObject Mommy;
-    public GameObject Vi;
-    public GameObject NoodleBoi;
+    public List<GameObject> Businessmen = new List<GameObject>();//Mommy, vi, demonessa
 
-    //Various Beats
-    public GameObject Rat;
-    public GameObject ShadowDoggo;
-    public GameObject Cricket;
-    public GameObject Lurker;
+    //Various Beasts
+    public List<GameObject> Beasts = new List<GameObject>();//rat, shadowdoggo, cricket, lurker
+
+    private int WhichBoss;//What boss is the player facing
+    private int BossStage;//How much damage has the boss taken already
+
+    private bool Prepare;
+    private bool ConversationMode;
+
+    public GameObject BossDefiner;
+
+
+    public GameObject Dimmer;
+    private float fadefloat;
+
+    public List<GameObject> HUDitems = new List<GameObject>();
+
+    private void Update()
+    {
+        //cheatcode: Press 0 during a bossfight to activate the dialogue. This will automatically trigger when the boss takes damage (when i implement it)
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            PreperationMode();
+        }
+
+        if (Prepare)//Dim
+        {
+            if(fadefloat <= 0.5f)
+            {
+                fadefloat += Time.deltaTime * 0.5f;
+
+                Color fadecolor = Dimmer.GetComponent<Renderer>().material.color;
+
+                fadecolor = new Color(fadecolor.r, fadecolor.g, fadecolor.b, fadefloat);
+                Dimmer.GetComponent<Renderer>().material.color = fadecolor;
+            }
+            else//Time stop
+            {
+                for(int i = 0; i < HUDitems.Count; i++)//Turn off HUD (remember to turn it back on
+                {
+                    HUDitems[i].SetActive(false);
+                }
+
+                Time.timeScale = 0;
+                Robot_Farmer.GetComponent<Animator>().Play("FarmerIntro");
+
+                if (WhichBoss == 1)
+                    Businessmen[BossStage - 1].GetComponent<Animator>().Play("EnemyIntro");
+                else if (WhichBoss == 2)
+                    Beasts[BossStage - 1].GetComponent<Animator>().Play("EnemyIntro");
+
+                ConversationMode = true;
+            }
+        }
+
+        if (ConversationMode)//Trigger the correct void
+        {
+            Prepare = false;
+
+            if (WhichBoss == 1)
+                CorporateDialogue();
+
+            if (WhichBoss == 2)
+                WardenDialogue();
+
+            if (WhichBoss == 3)
+                CorruptionDialogue();
+        }
+    }
+
+    public void PreperationMode()
+    {
+        WhichBoss = BossDefiner.GetComponent<NewCardHandScript>().RandomBoss;
+        BossStage++;
+
+        Prepare = true;
+    }
+
+    private void CorporateDialogue()
+    {
+        //BossStage++; Trigger this from the boss
+
+        if(BossStage == 1)//Mommy
+        {
+            Debug.Log("Now this works and i can do stuff here");
+        }
+        else if(BossStage == 2)//Vi
+        {
+
+        }
+        else//Stage 3 - Demoness
+        {
+
+        }
+    }
+
+
+    private void WardenDialogue()
+    {
+        BossStage++;
+
+        if (BossStage == 1)
+        {
+
+        }
+        else if (BossStage == 2)
+        {
+
+        }
+        else if (BossStage == 3)
+        {
+
+        }
+        else//Stage 4
+        {
+
+        }
+    }
+
+
+    private void CorruptionDialogue()
+    {
+        BossStage++;
+
+        if (BossStage == 1)
+        {
+
+        }
+        else if (BossStage == 2)
+        {
+
+        }
+        else//Stage 3
+        {
+
+        }
+    }
 }
